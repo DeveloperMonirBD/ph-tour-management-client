6bfb2dbc06a27ace4820401d0fe5dd2f8e2146e9
Monir <mrmonir0558@gmail.com>
2025-08-04 19:40:32 +0600

    git initilize and project setup almost done

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+
+export default function About() {
+  return (
+      <div className="py-4 px-4 container mx-auto">
+          <div>This is About Component</div>
+          <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum velit voluptate, molestiae porro officia excepturi maxime, nam quam quisquam placeat voluptas libero, cum rerum pariatur
+              mollitia nesciunt iusto eligendi iure!
+          </p>
+      </div>
+  );
+}
