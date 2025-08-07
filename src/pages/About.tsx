54df907f0c2bf9f7af5d99aa40bdb48e47184d70
Monir <mrmonir0558@gmail.com>
2025-08-07 23:21:20 +0600

    login , registration and authentication almost done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,15 @@
-
 export default function About() {
   return (
-      <div className="py-4 px-4 container mx-auto">
-          <div>This is About Component</div>
-          <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum velit voluptate, molestiae porro officia excepturi maxime, nam quam quisquam placeat voluptas libero, cum rerum pariatur
-              mollitia nesciunt iusto eligendi iure!
-          </p>
+    <>
+      <div className="py-16 px-4 container mx-auto">
+        <h1> This is About component </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, qui
+          eos. Architecto quisquam odit soluta fugit quos ab unde, voluptatem
+          eos necessitatibus nesciunt, in neque consequatur adipisci velit
+          accusantium illum.
+        </p>
       </div>
+    </>
   );
 }
